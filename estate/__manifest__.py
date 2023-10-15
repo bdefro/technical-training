0f00095dcfbc176f560fe2af3068f02778d5dd64
bat <baptiste.defrance.dev@gmail.com>
2023-10-15 20:55:54 +0200

    chapter 6 done!

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,13 @@
 {
     "name": "Estate",  # The name that will appear in the App list
+    "category": "Real Estate/Brokerage",
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base", "web"],  # dependencies
     "data": [
-
+        "security/ir.model.access.csv",
+        "views/estate_property_view.xml",
+        "views/estate_property_menu.xml",
     ],
     "installable": True,
     'license': 'LGPL-3',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class Property(models.Model):
     """ Estate Property
@@ -6,21 +7,33 @@ class Property(models.Model):
     _name = "estate_property"
     _description = "Model representing an estate property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="Unknown")
     description = fields.Text()
 
+    three_months = lambda self: fields.Datetime.now() + \
+relativedelta(months = 3)
+
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,
+                                    default=three_months)
 
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copy=False)
 
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Type',
-        selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+    garden_orientation = fields.Selection(string='Garden Orientation',
+        selection=[("north", "North"), ("south", "South"),
+                   ("east", "East"), ("west", "West")],
         help="Garden Orientation")
+
+    active = fields.Boolean(default=True)
+    state = fields.Selection(string='State',
+        selection=[("new", "New"), ("offer received", "Offer Received"),
+                   ("offer accepted", "Offer Accepted"), ("sold", "Sold"),
+                   ("cancelled", "Cancelled")],
+        help="State of the property")
